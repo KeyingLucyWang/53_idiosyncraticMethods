d55516456259d63c0db2f4ec9ccc936bc627d413
Lucy <kwang90@stuy.edu>
2019-03-17 14:10:29 -0400

    Accumulator class modified

--- Accumulator.java
+++ Accumulator.java
@@ -2,17 +2,23 @@
   Represent accumulating data from the elements in a
   List_inArraySlots that are of a particular type.
 
-- Java protects a programmer against applying a method to elements in list when some elements of the list might omit support for a particular operation.
+  3.  Stmbling block 0
+  Java protects a programmer against applying a method to
+  elements in list when some elements of the list might
+  omit support for a particular operation.
 
-  This protection is implemented by the __Java compiler__. The following attempt to violate the restriction is expected to result in the error _method not found in the given type___.
+  This protection is implemented by the __Java compiler__. 
+  The following code violates the restriction:
 
-  [your code]
   Object ele = list.get(i);
   if (ele.startsWith(prefix))
   //the compiler checks the variable type of ele for the method
       result += ele + " ";
 
-  [resulting error message]
+  [predicted error message]
+  The startsWith method is not found in the Object class, of which ele is an instance of.
+
+  [actual error message]
   error: cannot find symbol
   if (ele.startsWith(prefix))
 	           ^
@@ -22,21 +28,29 @@
 
 
 
-- A programmer should expect to be able to work around the stumbling block because the _JVM_ knows the type of an element.
+  4.  Workaround 0
+  A programmer should expect there to be a way to
+  work around the stumbling block, because
+  the ___JVM___ knows the type of an element.
 
-  Java's _instanceof__ operator identifies the type of an element to the _Java compiler_ 
+  Java's _instanceof__ operator identifies the type 
+  of an element to the _JVM_.
 
 
 
-- Use of the operator alone is insufficient because the __Java compiler___ (Java compiler? JVM?) continues to stumble over the previous stumbling block of protection against mis-applying a method. 
+  5. Stumbling block 1
+  However, use of the operator alone is insufficient,
+  because the __Java compiler___
+  objects to the following code that adds use of
+  the operator to the code from Stumbling block 0:
 
-[code that illustrates the use of the operator] 
     for (int i = 0; i < list.size(); i++){
-	Object ele = list.get(i);
+	   Object ele = list.get(i);
         if (ele instanceof String)
             if (ele.startsWith(prefix))
                 result += ele + " ";
-predicted error message: 
+
+[predicted error message] 
     startsWith() method is not found in Object class (ele's variable type)
 [resulting error message]
     error: cannot find symbol
@@ -46,14 +60,26 @@ predicted error message:
 		       location: variable ele of type Object
 
 
-Java provides a _casting_ facility that a programmer uses to tell the _Java compiler_ that they are using a subclass's method on an object for which a reference is stored in a super-type variable. 
-
+  6.  Workaround 1
+  Programmers use Java's ___cast____ operator
+  to tell the __compiler__
+  that code uses a subclass's method on an object,
+  even though the reference to the object is stored
+  in a super-class variable.
+ 
 [code that illustrates the use of the operator] 
     Object ele = list.get(i);
     if (((String)ele).startsWith(prefix))
 	    result += ele + " ";
 
-But use of that facility alone is insufficient because the _JVM_ stumbles over _some specific types of the Object in list cannot be casted to String_ (20–40 words). 
+  7. Stumbling block 2
+  But use of this operator alone is insufficient, 
+  because the __JVM__ objects to the following code that adds use of
+  the operator to the code from Stumbling block 0:
+
+  Object ele = list.get(i);
+    if (((String)ele).startsWith(prefix))
+      result += ele + " ";
 
 predicted error message: 
 the cast from Double to String is not applicable
@@ -69,8 +95,8 @@ A programmer can combine use of the operator and the facility to apply a method 
 [working code here, finally]
     Object ele = list.get(i);
 	    if (ele instanceof String)
-		if (((String)ele).startsWith(prefix))
-		    result += ele + " ";
+		    if (((String)ele).startsWith(prefix))
+		      result += ele + " ";
 	}
  */
 
@@ -85,15 +111,15 @@ public class Accumulator {
         List_inArraySlots list
       , String prefix
       ) {
-	String result = "";
-	for (int i = 0; i < list.size(); i++){
-	    Object ele = list.get(i);
-	    if (ele instanceof String)
-		if (((String)ele).startsWith(prefix))
-		    result += ele + " ";
-	}
+      String result = "";
+      for (int i = 0; i < list.size(); i++){
+        Object ele = list.get(i);
+        if (ele instanceof String)
+          if (((String)ele).startsWith(prefix))
+            result += ele + " ";
+        }
         return result; 
-    }
+      }
 
 
     /**
@@ -103,13 +129,13 @@ public class Accumulator {
     public static List_inArraySlots finites(
         List_inArraySlots list
       ) {
-	List_inArraySlots newList = new List_inArraySlots();
-	for (int i = 0; i < list.size(); i++){
-	    Object ele = list.get(i);
-	    if (ele instanceof Double)
-		if (!(((Double)ele).isInfinite()))
-		    newList.add(ele);
-	}
-	return newList;
+      List_inArraySlots newList = new List_inArraySlots();
+      for (int i = 0; i < list.size(); i++){
+        Object ele = list.get(i);
+        if (ele instanceof Double)
+          if (!(((Double)ele).isInfinite()))
+            newList.add(ele);
+        }
+        return newList;
+      }
     }
-}
